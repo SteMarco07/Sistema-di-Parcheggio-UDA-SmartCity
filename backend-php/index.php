0d6f6fbec86d978d12d50a6d127ec65b0c754761
PaoloRobolini <nonilmiopsn@gmail.com>
2026-02-11 11:41:59 +0100

    Documentazione e creazione rotte necessarie
    Rimozione di plates perché si visualizza tutto in frontend
    TODO: sistemazione disponibilità

--- backend-php/index.php
+++ backend-php/index.php
@@ -3,7 +3,6 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use League\Plates\Engine;
 use Util\Connection;
 
 require './vendor/autoload.php';
@@ -11,14 +10,10 @@ require './conf/config.php';
 
 $pdo = Connection::getInstance();
 
-//crea l'engine dei template
-$templates = new Engine('templates', 'tpl');
-
 $app = AppFactory::create();
-//$app->setBasePath('/prova_DB');
 
 $app->get('/', function (Request $request, Response $response, $args): Response {
-    global $templates, $pdo;
+    global $pdo;
 
     $pagina = $templates->render('routes', []);
     $response->getBody()->write($pagina);
@@ -26,57 +21,114 @@ $app->get('/', function (Request $request, Response $response, $args): Response 
     return $response;
 });
 
+// Restituisce tutti i parcheggi presenti
 $app->get('/park', function (Request $request, Response $response, $args): Response {
-    global $templates, $pdo;
+    global $pdo;
 
-    $pagina = $templates->render('park', []);
-    $response->getBody()->write($pagina);
+    //Restituisce le rotte in JSON, che verranno poi interpretate dal frontend
+    $response->getBody()->write($pagina)->withHeader('Content-Type', 'application/json');
 
     return $response;
 });
 
+// Restituisce un parcheggio specifico
 $app->get('/park/{park_id}', function (Request $request, Response $response, $args): Response {
-    global $templates, $pdo;
 
     $park_id = $args['park_id'];
 
-    $pagina = $templates->render('park', []);
-    $response->getBody()->write($pagina);
+    // Logica di selezione
+
+    if ($park) {
+        $response->getBody()->write(json_encode($park));
+    } else {
+        $response = $response->withStatus(404);
+    }
 
     return $response;
 });
 
-$app->post('/reservation/{park_id}', function (Request $request, Response $response, $args): Response {
+// Modifica una nuova prenotazione
+$app->post('/reservation', function (Request $request, Response $response, $args): Response {
+    global $pdo;
+
+    // Prende l'ID dal body
+    $park_id = $request->getParsedBody()['park_id'];
+
+    // Logica di modifica
+
+    return $response;
+});
+
+// L'amministratore deve poter modificare un parcheggio
+$app->put('/park', function (Request $request, Response $response, $args): Response {
+    global $pdo;
+
+    $park_id = $request->getParsedBody()['park_id'];
+
+    //Logica di modifica
+
+    return $response->withStatus(204);
+});
+
+// Crea una nuova presentazione (sia utente che admin)
+$app->put('/reservation', function (Request $request, Response $response, $args): Response {
     global $templates, $pdo;
 
-    $park_id = $args['park_id'];
+    $reservation_id = $request->getParsedBody()['reservation_id'];
 
     $pagina = $templates->render('park', []);
     $response->getBody()->write($pagina);
 
-    return $response;
+    return $response->withStatus(201);
 });
 
-$app->put('/reservation/{reservation_id}', function (Request $request, Response $response, $args): Response {
-    global $templates, $pdo;
+// L'amministratore deve poter creare un parcheggio
+$app->put('/park', function (Request $request, Response $response, $args): Response {
+    global $pdo;
 
-    $reservation_id = $args['reservation_id'];
+    $park_id = $request->getParsedBody()['park_id'];
 
-    $pagina = $templates->render('park', []);
-    $response->getBody()->write($pagina);
+    //Logica di creazione
 
-    return $response;
+    return $response->withStatus(201);
 });
 
+// Elimina una prenotazione, dal lato utente
 $app->delete('/reservation/{reservation_id}', function (Request $request, Response $response, $args): Response {
-    global $templates, $pdo;
+    global $pdo;
 
     $reservation_id = $args['reservation_id'];
 
-    $pagina = $templates->render('park', []);
-    $response->getBody()->write($pagina);
+    // Logica di eliminazione
 
+    $response = $response->withStatus(204);
     return $response;
+});
+
+// L'amministratore deve poter eliminare un parcheggio
+$app->delete('/park/{park_id}', function (Request $request, Response $response, $args): Response {
+    global $pdo;
+
+    $park_id = $args['park_id'];
+
+    // Logica di eliminazione
+
+    $response = $response->withStatus(204);
+    return $response;
+});
+
+// Restituisce i posti disponibili prima che l'utente ne faccia una
+$app->get('/reservation/available/{start}/{end}', function (Request $request, Response $response, $args): Response {
+    global $pdo;
+
+    $data_inizio = $args['start'];
+    $data_fine = $args['end'];
+
+    // Logica per verificare la disponibilità delle prenotazioni
+    $disponibili = []; // Array di prenotazioni disponibili
+
+    $response->getBody()->write(json_encode($disponibili));
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();

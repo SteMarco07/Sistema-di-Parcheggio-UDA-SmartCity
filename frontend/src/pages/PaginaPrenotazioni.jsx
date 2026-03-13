a9240cc968621779d4ddc700d9e802971c85cfe0
matteofogazzi <matteoeffe07@gmail.com>
2026-03-13 09:20:33 +0100

    tolto il pulsante add e relativo form

--- frontend/src/pages/PaginaPrenotazioni.jsx
+++ frontend/src/pages/PaginaPrenotazioni.jsx
@@ -10,29 +10,7 @@ function PaginaPrenotazioni() {
       {/* Titolo + bottone */}
       <div className="flex justify-between items-center">
         <h2 className="text-2xl font-semibold mb-4">Prenotazioni</h2>
-        <button
-          type="button"
-          onClick={addFieldset} // chiama la funzione dello store
-          className="btn btn-soft"
-        >
-          Add
-        </button>
       </div>
-
-      {/* Fieldset dinamici */}
-      {fieldsets?.map((field, index) => (
-        <fieldset
-          key={field.id}
-          className="fieldset bg-base-200 border border-base-300 p-4 rounded-box mb-4"
-        >
-          <legend className="fieldset-legend">Nuovo Campo {index + 1}</legend>
-          <input
-            type="text"
-            className="input input-bordered w-full"
-            placeholder="Scrivi qualcosa..."
-          />
-        </fieldset>
-      ))}
 
       {/* Lista prenotazioni */}
       {prenotazioni.length === 0 ? (

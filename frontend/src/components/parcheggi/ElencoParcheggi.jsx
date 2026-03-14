08a039e79c870275bddb1cc6f74c8614e4f7f0c1
PaoloRobolini <nonilmiopsn@gmail.com>
2026-03-14 11:26:37 +0100

    Mostra un messaggio quando non ci sono parcheggi disponibili

--- frontend/src/components/parcheggi/ElencoParcheggi.jsx
+++ frontend/src/components/parcheggi/ElencoParcheggi.jsx
@@ -14,13 +14,18 @@ function ElencoParcheggi() {
           <h2 className="text-3xl font-semibold">Parcheggi disponibili</h2>
         </div>
         <div className='overflow-y-auto flex-1'>
-          {parcheggiFiltrati.map((item) => (
+          {parcheggiFiltrati.length !== 0 ? (parcheggiFiltrati.map((item) => (
             <div key={item.id} className="card card-border w-full bg-base-90 shadow-xl mb-4">
               <div className="card-body">
                 <ParcheggioCard parcheggio={item} />
               </div>
             </div>
-          ))}
+          ))) : (
+            <div className="flex flex-col items-center justify-center h-full text-gray-500 mx-5">
+              <p className="text-lg text-center">Nessun parcheggio disponibile per i criteri selezionati.</p>
+              <p className="text-sm mt-2">Prova a modificare la tua ricerca o controlla più tardi.</p>
+            </div>
+          )}
         </div>
 
       </div>

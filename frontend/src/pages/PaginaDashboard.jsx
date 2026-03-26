ded08329c4d476525cb7f39c68af3d0a0a9ea8ca
PaoloRobolini <nonilmiopsn@gmail.com>
2026-03-26 16:48:32 +0100

    organizzazione senza grafici

--- frontend/src/pages/PaginaDashboard.jsx
+++ frontend/src/pages/PaginaDashboard.jsx
@@ -3,14 +3,50 @@ import { useStore } from '../store.jsx';
 
 import TableParcheggi from '../components/dashboard/TableParcheggi.jsx';
 
+import { BarChart } from '@mui/x-charts/BarChart';
+
 function PaginaDashboard() {
     const { parcheggi } = useStore();
 
     return (
-        <div className="p-4">
+        <div className="p-4 flex flex-col items-center">
             <h1 className="text-4xl font-bold mb-4">Dashboard</h1>
 
-            <TableParcheggi />
+            <div className="join join-horizontal gap-6">
+                <div className="join join-vertical gap-6 w-[75%]">
+                    <div className="bg-white rounded-lg shadow-md p-6">
+                        <TableParcheggi />
+                    </div>
+
+                    <div className="bg-white rounded-lg shadow-md p-6">
+                        <TableParcheggi />
+                    </div>
+
+
+                </div>
+
+                <div className="w-[20%] bg-white rounded-lg shadow-md p-6">
+
+                    <BarChart
+                        xAxis={[
+                            {
+                                id: 'barCategories',
+                                data: ['bar A', 'bar B', 'bar C'],
+                                height: 28,
+                            },
+                        ]}
+                        series={[
+                            {
+                                data: [2, 5, 3],
+                            },
+                        ]}
+                        height={300}
+                    />
+
+                </div>
+            </div>
+
+
 
         </div>
     );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [
     react(),
-    // Add any additional plugins here
     tailwindcss()
-
+    
   ],
 })

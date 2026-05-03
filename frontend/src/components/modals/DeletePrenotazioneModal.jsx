e693d840167b20fd70ad789c6c15401c0039385a
SteMarco07 <marcoste2007@gmail.com>
2026-05-03 20:36:25 +0200

    Aggiunto nome utente
    È stato aggiunto il nome dell'utente nel modale di eliminazione di una prenotazione, solamente lato dashboard admin

--- frontend/src/components/modals/DeletePrenotazioneModal.jsx
+++ frontend/src/components/modals/DeletePrenotazioneModal.jsx
@@ -15,24 +15,29 @@ export default function DeletePrenotazioneModal({ open, onClose, prenotazione, s
   }
 
 
-return (
-  <div className={"modal " + (open ? 'modal-open' : '')}>
-    <div className="modal-box">
-      <h3 className="font-bold text-lg">Conferma eliminazione prenotazione</h3>
-      <p className="py-2">Sei sicuro di voler eliminare la seguente prenotazione?</p>
+  return (
+    <div className={"modal " + (open ? 'modal-open' : '')}>
+      <div className="modal-box">
+        <h3 className="font-bold text-lg">Conferma eliminazione prenotazione</h3>
+        <p className="py-2">Sei sicuro di voler eliminare la seguente prenotazione?</p>
 
-      <div className="space-y-2 text-sm">
-        {showUser && <div><strong>Utente:</strong> {prenotazione.id_user}</div>}
-        <div><strong>Parcheggio:</strong> {prenotazione.parking_name}</div>
-        <div><strong>Inizio:</strong> {prenotazione.start_time}</div>
-        <div><strong>Fine:</strong> {prenotazione.end_time}</div>
-      </div>
+        <div className="space-y-2 text-sm">
+          {showUser &&
+            <>
+              <div><strong>ID utente:</strong> {prenotazione.id_user}</div>
+              <div><strong>Nome utente:</strong> {prenotazione.user_first_name} {prenotazione.user_last_name}</div>
+            </>
+          }
+          <div><strong>Parcheggio:</strong> {prenotazione.parking_name}</div>
+          <div><strong>Inizio:</strong> {prenotazione.start_time}</div>
+          <div><strong>Fine:</strong> {prenotazione.end_time}</div>
+        </div>
 
-      <div className="modal-action mt-4">
-        <button className="btn" onClick={onClose}>Annulla</button>
-        <button className="btn btn-error" onClick={() => onConfirm(prenotazione)}>Elimina</button>
+        <div className="modal-action mt-4">
+          <button className="btn" onClick={onClose}>Annulla</button>
+          <button className="btn btn-error" onClick={() => onConfirm(prenotazione)}>Elimina</button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
